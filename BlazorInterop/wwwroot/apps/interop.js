5d338a8248fe6de821e4bcfa0b19797f071d3fb9
Amir Zuker <a.zuker@gmail.com>
2020-06-21 23:01:23 +0300

    baseline

---
+++ BlazorInterop/wwwroot/apps/interop.js
@@ -0,0 +1,9 @@
+﻿window.interopSamples = {
+    subscribeToProductChange: function(elem, ref) {
+        elem.addEventListener('productSelected',
+            p => ref.invokeMethodAsync('SelectProduct', p.detail));
+    },
+    updateProduct: function (elem, product) {
+        elem.product = product;
+    }
+};
